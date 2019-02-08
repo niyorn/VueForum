f135d79bbabd7ded1e14a45b90c476f31c434ec7
niyorn <c.niyornram@hotmail.com>
2019-02-08 15:51:23 +0100

    Add routing

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div class="thread-list">
+        <h2 class="list-title">Threads</h2>
+        <ThreadListItem
+            v-for="thread in threads" :key="thread.id"
+            :thread = thread
+        />
+    </div>
+
+</template>
+
+<script>
+    import ThreadListItem from './ThreadListItem.vue'
+
+    export default {
+        name: 'ThreadList',
+
+
+        components: {
+            ThreadListItem
+        },
+
+
+        props: {
+            threads: {
+                required: true,
+                type: Array
+            }
+        }
+    }
+</script>
---
+++ src/components/ThreadShow.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="col-large push-top">
+        <h1>{{thread.title}}</h1>
+        <div class="post-list">
+            <div v-for="postId in thread.posts" :key="postId.id" class="post">
+                <div class="user-info">
+                    <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+                    <a href="#">
+                        <img class="avatar-large" src="http://i.imgur.com/s0AzOkO.png" alt="">
+                    </a>
+
+                    <p class="desktop-only text-small">107 posts</p>
+                </div>
+
+                <div class="post-content">
+                    <div>
+                        <p>
+                            Is horseradish and Wasabi the same thing? I've heard so many different things.<br><br>
+                            I want to know once and for all.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="post-date text-faded">
+                    6 hours ago
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import sourceData from '@/data.json'
+
+    export default {
+        props: {
+            id: {
+                required: true,
+                type: String
+            }
+        },
+
+
+        data() {
+            return {
+                thread: sourceData.threads[this.id],
+                posts: sourceData.posts,
+                users: sourceData.users
+            }
+        }
+    }
+</script>
