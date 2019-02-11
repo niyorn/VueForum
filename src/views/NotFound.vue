1a04ce5700fb1fac5b608a2049eed918cff5a30b
niyorn <c.niyornram@hotmail.com>
2019-02-11 14:50:41 +0100

    Add css module

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="col-full">
-        <h1>Not Found</h1>
-        <p>We couldn't find the page you're looking for. You wanna go <router-link :to="{name:'Home'}">Home</router-link> instead?</p>
+    <div class="col-full" :class="$style.centered">
+        <h1 :class="$style.title">Not Found</h1>
+        <p :class="$style.text">We couldn't find the page you're looking for. You wanna go <router-link :to="{name:'Home'}">Home</router-link> instead?</p>
     </div>
 </template>
 
-<style lang="scss" scoped>
-    div {
+<style lang="scss" module>
+    .centered {
         text-align: center;
     }
 
-    h1 {
+    .title {
         font-size: 6em;
     }
 
-    p {
+    .text {
         font-size: 4em;
         font-weight: lighter;
     }
